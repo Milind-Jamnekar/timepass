a9761863f09feddfc808a26546ea113e09c6469b
Milind Jamnekar <milindjamnekar502@gmail.com>
2021-09-15 19:45:55 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// import express from "express";
+// import path, { dirname } from "path";
+
+const express = require("express");
+
+const app = express();
+
+app.use("/static", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("public/index.html", { root: __dirname });
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile("views/login.html", { root: __dirname });
+});
+
+app.listen(3000, () => console.log("listening To port 3000"));
